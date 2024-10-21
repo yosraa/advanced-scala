3bbea09aa1d00d1b225cedfd5d191d23b277b279
yaddali <yosraaddali@gmail.com>
2024-10-21 17:51:11 +0200

    Implementation de la fonction firstUniqChar

--- src/main/scala/LeetCode.scala
+++ src/main/scala/LeetCode.scala
@@ -18,9 +18,49 @@ object LeetCode {
     }
    }
   true
+   /*
+   val stringCoded1 = s.map(c => s.indexOf(c)).toList
+   val stringCoded2 = t.map(c => t.indexOf(c)).toList
+   return stringCoded1 == stringCoded2
+
+    */
+   /*
+   val lst1 = new Array[Int](s.length)
+   val lst2 = new Array[Int](t.length)
+   for (i <- s.indices){
+     lst1(i) = s.indexOf(s(i))
+     lst2(i) = t.indexOf(t(i))
+   }
+   lst1 == lst2
+
+    */
+
  }
+  def firstUniqChar(s:String): Int ={
+
+    val frequency = scala.collection.mutable.Map[Char, Int]()
+    // compter la frequence de chaque caractère
+    for(char <- s){
+      frequency(char) = frequency.getOrElse(char,0) + 1
+    }
+    // trouver l'index du premier caractère unique
+   /*
+    for (i <- s.indices){
+      if (frequency(s(i)) == 1){
+        return i
+      }
+    }
+    // Si aucun caractère unique n'est trouvé, retourner -1
+    -1
+
+    */
+    s.indices.find(i => frequency(s(i)) == 1).getOrElse(-1)
+  }
+
 def main(args:Array[String]): Unit= {
- isIsomorphic("egg", "add")
+  isIsomorphic("egg", "add")
+  val input = "leetcode"
+  println(firstUniqChar(input))
 }
 
 }
