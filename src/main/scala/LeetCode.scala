b388f208cd215baaa0786bb52a3c86cfa677c2e7
yaddali <yosraaddali@gmail.com>
2024-10-23 15:42:31 +0200

    Ajout de la focntion threeSum

--- src/main/scala/LeetCode.scala
+++ src/main/scala/LeetCode.scala
@@ -56,11 +56,48 @@ object LeetCode {
     */
     s.indices.find(i => frequency(s(i)) == 1).getOrElse(-1)
   }
+  def threSum(nums: Array[Int]): List[List[Int]]= {
+    if ( nums.length < 3) return List.empty
+    var res = Set[List[Int]]()
+    val sortedNums = nums.sorted
+    for (i <- sortedNums.indices){
+      // sauter les doublons pour la première valeur
+      if(i> 0 && sortedNums(i) == sortedNums(i-1)){
+
+      } else{
+        var j = i+1
+        var k =sortedNums.length - 1
+        while ( j< k){
+          val total = sortedNums(i) + sortedNums(j) + sortedNums(k)
+          if (total > 0){
+            k-=1
+          }else if (total < 0){
+            j+=1
+          }else{
+            // ajout du triplet à l'ensemble(set)
+            res += List(sortedNums(i), sortedNums(j), sortedNums(k))
+            j += 1
+            k -= 1
+            // éviter les doublons
+            while(j < k && sortedNums(j) == sortedNums(j-1)){
+              j+=1
+            }
+            while (j < k && sortedNums(k) == sortedNums(k+1)){
+              k-=1
+            }
+          }
+        }
+      }
+    }
+    res.toList
+  }
 
 def main(args:Array[String]): Unit= {
   isIsomorphic("egg", "add")
   val input = "leetcode"
   println(firstUniqChar(input))
+
+  println(threSum(Array(-1,0,1,2,-1,-4)))
 }
 
 }
